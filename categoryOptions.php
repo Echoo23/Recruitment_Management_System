4926ac5267bd36490dd7cc7b2eca4a0a2f9ac08d
Paul Jerico <125523684+Echoo23@users.noreply.github.com>
2025-06-28 15:22:28 +0800

    Add files via upload

---
+++ categoryOptions.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<option>- All Job Postings -</option>
+<option>Office staff</option>
+<option>Finance manage</option>
+<option>Admin assistant</option>
+<option>Liason Officer</option>
---
+++ skillscategory.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<option>- Skills category - </option>
+<option>Office staff</option>
+<option>Finance manage</option>
+<option>Admin assistant</option>
+<option>Liason Officer</option>
+
+
